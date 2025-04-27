c7ddc4af11e7e9f48a3618a792b5806407d94af8
wunisumnima <sumnima216@gmail.com>
2025-04-27 14:44:52 +0000

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -3,10 +3,11 @@ const mysql = require("mysql2");
 require("dotenv").config();
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Goodlife@12345',
-  database: 'campus_booking',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306
 });
 
 db.connect(err => {
